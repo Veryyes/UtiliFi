054f5a28871e3cd746170fd0d80807df03840ed0
Brandon Wong <Veryyes10@yahoo.com>
2018-02-18 12:53:01 +0000

    Database pushing yes!

--- pi.py
+++ pi.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
 import serial
+import web
+import time
 
 output = ""
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 ser.flushInput()
+
+f = open("sqlconnect", "r")
+hostname = f.readline().strip()
+username = f.readline().strip()
+password = f.readline().strip()
+f.close()
+
+db = web.database(dbn='mysql',host=hostname, port= 3306 , user=username, pw=password, db="utilify" )
 
 i = 0
 buff = []
@@ -18,7 +28,11 @@ while True:
 			buff.append(input)
 			i += 1
 	if(len(buff) == 7):						#check for complete list
+		date = time.strftime('%Y-%m-%d %X')
 		print buff
+		db.insert('gas', time=date, gasFlow= buff[1], gasTotal=buff[2] )
+		db.insert('water', time=date, waterFlow=buff[3], waterTotal=buff[4])
+		db.insert('power', time=date, watts=buff[5], energy=buff[6])
 		buff = []
 	else:
 		print "SKIP " + str(buff)
